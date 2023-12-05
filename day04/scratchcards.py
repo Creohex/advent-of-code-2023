d117bbae93369e8ac3693a05f4e19f6699f063ac
Creohex <paradise.source@gmail.com>
2023-12-05 18:20:35 +0300

    simplify day 4

--- day04/scratchcards.py
+++ day04/scratchcards.py
@@ -1,32 +1,24 @@
 #!python
 
-import re
-
-
 def count_points(cards):
     points = 0
     for winning, actual in cards:
         amount = len(winning.intersection(actual))
-        if amount == 1:
-            points += 1
-        elif amount > 1:
-            points += 2 ** (amount - 1)
+        points += 1 if amount == 1 else 2 ** (amount - 1) if amount > 1 else 0
     return points
 
 def count_compounding_cards(cards):
-    cards_in_play = [1 for _ in range(len(cards))]
-
+    cards_in_play = [1 for _ in cards]
     for i, (winning, actual) in enumerate(cards):
         amount_won = len(winning.intersection(actual))
         if amount_won:
             for di in range(i + 1, i + 1 + amount_won):
                 cards_in_play[di] += cards_in_play[i]
-
     return sum(cards_in_play)
 
 
 with open("./input", "r") as f:
-    cards = [tuple(map(set, map(lambda c: re.findall(r"\d+", c),
+    cards = [tuple(map(set, map(lambda c: c.strip().split(),
                                 card.split(":")[1].split("|"))))
             for card in f.readlines()]
 

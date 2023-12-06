846acdd02ea3af3ce37eff2ab892ca771f7fbf49
Creohex <paradise.source@gmail.com>
2023-12-06 20:44:06 +0300

    minor day 6 upd

--- day06/wait-for-it.py
+++ day06/wait-for-it.py
@@ -1,6 +1,8 @@
 #!python
 
 import re
+from typing import Iterable
+
 
 test = """\
 Time:      7  15   30
@@ -13,22 +15,21 @@ Distance:   446   1292   1035   1007
 """
 
 
-def parse_input(source: str, fixed_kerning=False):
+def parse_input(source: str, fixed_kerning: bool=False) -> Iterable[tuple]:
     vals = list(map(int, re.findall(r"\d+", source.replace(" ", "")
                                     if fixed_kerning else source)))
     return zip(vals[:len(vals)//2], vals[len(vals)//2:])
 
-def solve(rounds, result=1):
+def solve(rounds: Iterable[tuple]) -> int:
+    result = 1
     for time, target_distance in rounds:
         speed = 0
         times_over_record = 0
-
         for dt in range(time):
             if speed * (time - dt) > target_distance:
                 times_over_record += 1
             speed += 1
         result *= times_over_record
-
     return result
 
 

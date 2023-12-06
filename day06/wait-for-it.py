6ffe7c6a0b40d273d7cd98c0eeca85acd18b9f7b
Creohex <paradise.source@gmail.com>
2023-12-06 20:14:03 +0300

    add day 6

---
+++ day06/wait-for-it.py
@@ -0,0 +1,39 @@
+#!python
+
+import re
+
+test = """\
+Time:      7  15   30
+Distance:  9  40  200
+"""
+
+data = """\
+Time:        54     81     70     88
+Distance:   446   1292   1035   1007
+"""
+
+
+def parse_input(source: str, fixed_kerning=False):
+    vals = list(map(int, re.findall(r"\d+", source.replace(" ", "")
+                                    if fixed_kerning else source)))
+    return zip(vals[:len(vals)//2], vals[len(vals)//2:])
+
+def solve(rounds, result=1):
+    for time, target_distance in rounds:
+        speed = 0
+        times_over_record = 0
+
+        for dt in range(time):
+            if speed * (time - dt) > target_distance:
+                times_over_record += 1
+            speed += 1
+        result *= times_over_record
+
+    return result
+
+
+# part 1:
+print(solve(parse_input(data)))
+
+# part 2:
+print(solve(parse_input(data, fixed_kerning=True)))
